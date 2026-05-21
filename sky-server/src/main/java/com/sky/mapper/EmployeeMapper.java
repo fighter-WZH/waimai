a1a248b74268cbe8bca3b7e663fd5dd917a50634
wzh <2435214624@qq.com>
2026-05-21 11:15:03 +0800

    苍穹外卖

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.data.redis.connection.ReactiveListCommands;
 
 @Mapper
 public interface EmployeeMapper {
@@ -14,5 +16,15 @@ public interface EmployeeMapper {
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
+
+    /**
+     * 插入员工数据
+     * @param employee
+     */
+    @Insert("insert into employee (name,username,password,phone,sex,id_number,create_time,update_time,create_user,update_user,status )" +
+            "value" +
+            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
+    public void insert(Employee employee);
+
 
 }
